1ef0fab1da79ba400a770dfddd9bad6dd8b17758
spring <365246692@qq.com>
2023-01-15 16:30:17 +0800

    文件写入支持&新模板

--- src/util.js
+++ src/util.js
@@ -1,9 +1,12 @@
 import fs from 'fs';
+import os from "os";
 import TextToSVG from 'text-to-svg';
 import axios from "axios";
 import exports from 'convert-svg-to-png';
 
 const svgToPng = async (svg) => await exports.convert(svg);
+
+const UTF8Encoder = new TextEncoder('utf8');
 
 const textToSVGRegular = TextToSVG.loadSync("font/Torus-SemiBold.ttf");
 
@@ -73,4 +76,51 @@ export function replaceText(text = '', rep = '', reg = /.*/) {
 export function getBase64Text(buffer) {
     let data = Buffer.from(buffer, 'binary').toString('base64');
     return 'data:image/png;base64,' + data;
+}
+
+export function randomString(e) {
+    e = e || 32;
+    var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
+        a = t.length,
+        n = "";
+    for (let i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+    return n
+}
+
+export class SaveFiles{
+    files = [];
+    tmpDir = '';
+    constructor() {
+        let tmp = randomString(6);
+        while (fs.existsSync(`${os.tmpdir()}/${tmp}`)){
+            tmp = randomString(6);
+        }
+        this.tmpDir = `${os.tmpdir()}/${tmp}/`;
+        fs.mkdirSync(this.tmpDir);
+    };
+    saveSvgText(text){
+        let f = randomString(4)+'.svg';
+        while (this.files.includes(f)) {
+            f = randomString(4)+'.svg';
+        }
+        this.files.push(f);
+
+        fs.writeFileSync(this.tmpDir + f, UTF8Encoder.encode(text));
+    };
+    save(file){
+        let f = randomString(4);
+        while (this.files.includes(f)) {
+            f = randomString(4);
+        }
+        this.files.push(f);
+        fs.writeFileSync(this.tmpDir + f, file);
+    };
+
+    getAllPath(){
+        return this.files.map(f => this.tmpDir + f);
+    };
+
+    remove() {
+        fs.rmSync(this.tmpDir, {recursive: true});
+    };
 }
