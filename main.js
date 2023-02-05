77947b5da7422c09b533a62880f7f91fc6e30e1b
spring <365246692@qq.com>
2023-02-06 00:33:17 +0800

    增加测试,调整格式

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 import fs from "fs";
 import express from "express";
-import os from "os";
 import formidable from "express-formidable";
-import {card_D, card_H, card_A1, Panel_H} from "./src/card.js";
-import {readImage} from "./src/util.js";
+import {card_A1, card_D, card_H, Panel_H} from "./src/card.js";
+import {CACHE_PATH, getFlagSvg, readImage} from "./src/util.js";
 
+console.time("1")
 fs.writeFileSync("image/out/cardA1.png", await card_A1());
+console.timeEnd("1")
+
+console.time("2")
 fs.writeFileSync("image/out/cardD.png", await card_D());
+console.timeEnd("2")
+
+console.time("3")
 fs.writeFileSync("image/out/cardH.png", await card_H());
+console.timeEnd("3")
+
+console.time("4")
 fs.writeFileSync("image/out/panel_H.png", await Panel_H());
+console.timeEnd("4")
+
+console.time("5")
+fs.writeFileSync("image/out/cn.svg", await getFlagSvg("CN"));
+console.timeEnd("5")
 
 const app = express();
 app.use(formidable({
     encoding: 'utf-8',
-    uploadDir: os.tmpdir(),
+    uploadDir: CACHE_PATH,
     autoClean: true,
     multiples: true,
 }));
 
 app.post('*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method' )
+    res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE')
     res.header('Allow', 'POST')
     next();
@@ -50,13 +64,14 @@ app.post('/card-H1', async (req, res) => {
 
 app.listen(8555, () => {
     console.log('ok - http://localhost:8555');
-    console.log(os.tmpdir())
+    console.log(CACHE_PATH)
 })
+
 // form: data:text ... img:file
-function checkData(req, files=['']) {
+function checkData(req, files = ['']) {
     let fs = {};
     for (const file of files) {
-        if (req.files[file]){
+        if (req.files[file]) {
             fs[file] = readImage(req.files[file].path);
         }
     }
@@ -65,6 +80,7 @@ function checkData(req, files=['']) {
         ...fs,
     };
 }
+
 //  form data: json text{ xxx: xxx} img: file ...
 function checkJsonData(req) {
     let json = JSON.parse(req.fields['json']);
