d8dd4b174abcfb2a7841a170e1435dd16c92eb24
spring <365246692@qq.com>
2023-02-10 16:04:47 +0800

    x

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import express from "express";
 import formidable from "express-formidable";
 import {card_D, card_H} from "./src/card.js";
 import {CACHE_PATH, readImage} from "./src/util.js";
+import {panel_E} from "./src/panel.js";
 
 /*
 //    已经部署在机器上了,提交前请注释掉测试代码
@@ -44,6 +45,18 @@ app.post('/card-H1', async (req, res) => {
     try {
         const f = checkJsonData(req, ["background", "avatar"]);
         const png = await card_H(f);
+        res.set('Content-Type', 'image/png');
+        res.send(png);
+    } catch (e) {
+        res.status(500).send(e.stack);
+    }
+})
+
+//**************************************************** panel ****************************
+app.post('/panel_E', async (req, res) => {
+    try {
+        const f = checkJsonData(req);
+        const png = await panel_E(f);
         res.set('Content-Type', 'image/png');
         res.send(png);
     } catch (e) {
