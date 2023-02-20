f8e7229352b483ef3713a27c5d2247b01a903d57
spring <365246692@qq.com>
2023-02-20 13:23:37 +0800

    修改了一下

--- src/component/label.js
+++ src/component/label.js
@@ -1,10 +1,4 @@
-import {
-    getExportFileV3Path,
-    readTemplate,
-    implantImage,
-    replaceText,
-    torus
-} from "../util.js";
+import {implantImage, replaceText, torus} from "../util.js";
 
 export const LABEL_OPTION = {
     ACC: {
@@ -98,16 +92,8 @@ export async function label_E(data = {
     let reg_text = /(?<=<g id="Text">)/;
     let reg_icon = '${icon}';
 
-    // 读取模板
-    let svg = readTemplate('template/Label_E.svg');
-
     // 文字的 <path>
     let icon_title = torus.getTextPath(data.icon_title, 50, 14.88, 18, "left baseline", "#a1a1a1");
-
-    if (data.remark) {
-        let remark = torus.getTextPath(data.remark, 200, 14.88, 18, "right baseline", "#646464");
-        svg = replaceText(svg, remark, reg_text);
-    }
 
     let datas = torus.getTextPath(data.data_b, 56, 44.75, 36, "left baseline", "#fff");
 
@@ -116,12 +102,41 @@ export async function label_E(data = {
         datas = datas + torus.getTextPath(data.data_m, 56 + data_b_x.width, 44.75, 24, "left baseline", "#fff");
     }
 
-    // 替换模板内容,replaceText(模板, 内容, 正则)
-    svg = replaceText(svg, icon_title, reg_text);
-    svg = replaceText(svg, datas, reg_text);
-    svg = implantImage(svg,50,50,0,0,1,`${data.icon}`,reg_text)
+    // 尽量减少文件的读取,少量文字的模板请直接写在代码里(是少量的,东西多了还是要读取模板)
+    let svg = `
+        <defs>
+        </defs>
+        <g id="Icon">
+        </g>
+        <g id="Text">
+            ${icon_title}
+            ${datas}
+        </g>
+    `;
+
+    if (data.remark) {
+        let remark = torus.getTextPath(data.remark, 200, 14.88, 18, "right baseline", "#646464");
+        svg = replaceText(svg, remark, reg_text);
+    }
+    // ${data.icon} 就是字符串化 data.icon ,另外可能你的参数层级出了问题,看下面的注释
+    svg = implantImage(svg, 50, 50, 0, 0, 1, data.icon, reg_text)// 注意 LABEL_OPTION.xx = {icon:"xx.jpg", icon_title: "XX"}
+    // 所以此方法的默认参数是 {icon: {icon:"xx.jpg", icon_title: "ACC"}, icon_title: "ACC"}
+    // 而你大部分的传入是 {icon:"xx.jpg", icon_title: "ACC"}
+    // 我猜测你想展开到data里,看最底下的注释
 
     // let out_svg = new InsertSvgBuilder(svg).insertImage(data.icon, reg_icon);
 
     return svg.toString();
 }
+
+// ******************************************************************************* 如果你想将 LABEL_OPTION.xx 展开到参数里,请使用下面的方法
+
+export async function _label_E(data = {
+    ...LABEL_OPTION.RKS,  //                                                    <--使用这种方式
+    data_b: '98',
+    data_m: '.36%',
+}, reuse = false) {
+    // todo
+}
+
+// 这样就  data = {icon:"xx.jpg", icon_title: "XX", remark:'xxx', data_b:'', data_m:''}
