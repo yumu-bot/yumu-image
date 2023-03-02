57a94f45873da0e2ce59a8d96f77b01a694c98f3
spring <365246692@qq.com>
2023-03-02 20:59:12 +0800

    117 行

--- src/panel/panel_F.js
+++ src/panel/panel_F.js
@@ -1,10 +1,14 @@
 import {
     getMatchNameSplitted,
     getNowTimeStamp,
-    getRandomBannerPath, getStarRatingObject,
+    getRandomBannerPath,
+    getStarRatingObject,
     implantImage,
-    implantSvgBody, InsertSvgBuilder, readTemplate,
-    replaceText, torus
+    implantSvgBody,
+    InsertSvgBuilder,
+    readTemplate,
+    replaceText,
+    torus
 } from "../util.js";
 import {card_A2} from "../card/card_A2.js";
 import {label_F1} from "../component/label.js";
@@ -114,6 +118,67 @@ export async function panel_F (data = {
     index_panel_name: 'MN v3.6',
 
 }, reuse = false) {
+    const scores = [
+        {
+            red: [
+                {
+                    player_name: 'na-gi', //妈的 为什么get match不给用户名啊
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 464277,
+                    player_mods: [],
+                    player_rank: 1, //一局比赛里的分数排名，1v1或者team都一样
+                },
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 412096,
+                    player_mods: [],
+                    player_rank: 2
+                },
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 268397,
+                    player_mods: [],
+                    player_rank: 6,
+                },
+            ],
+            blue: [
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 403437,
+                    player_mods: ['HD'],
+                    player_rank: 3,
+                },
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 371937,
+                    player_mods: [],
+                    player_rank: 4,
+                },
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 371007,
+                    player_mods: [],
+                    player_rank: 5,
+                },
+            ],
+            none: [
+                {
+                    player_name: '- Rainbow -',
+                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+                    player_score: 403437,
+                    player_mods: ['HD'],
+                    player_rank: 3,
+                },
+            ],
+        },
+    ]
+
+
     // 导入模板
     let svg = readTemplate('template/Panel_F.svg');
 
