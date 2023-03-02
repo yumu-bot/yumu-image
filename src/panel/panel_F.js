ac14f0955d233047bddc744b1d6b97338c9932a6
YuriMumu <1340691940@qq.com>
2023-03-02 21:34:47 +0800

    小改动

--- src/panel/panel_F.js
+++ src/panel/panel_F.js
@@ -58,59 +58,51 @@ export async function panel_F (data = {
         },
     },
     // F卡 events
-    scores: {
-        0: {
-            red: {
-                0: {
-                    player_name: 'na-gi', //妈的 为什么get match不给用户名啊
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 464277,
-                    player_mods: [],
-                    player_rank: 1, //一局比赛里的分数排名，1v1或者team都一样
-                },
-                1:{
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 412096,
-                    player_mods: [],
-                    player_rank: 2
-               },
-               2:{
-                   player_name: '- Rainbow -',
-                   player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                   player_score: 268397,
-                   player_mods: [],
-                   player_rank: 6,
-               },
-            },
-            blue: {
-                0: {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 403437,
-                    player_mods: ['HD'],
-                    player_rank: 3,
-                },
-                1:{
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 371937,
-                    player_mods: [],
-                    player_rank: 4,
-                },
-                2:{
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 371007,
-                    player_mods: [],
-                    player_rank: 5,
-                },
-            },
-            none: {
+    scores: [{
+        red: [{
+            player_name: 'na-gi', //妈的 为什么get match不给用户名啊
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 464277,
+            player_mods: [],
+            player_rank: 1, //一局比赛里的分数排名，1v1或者team都一样
+        }, {
+            player_name: '- Rainbow -',
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 412096,
+            player_mods: [],
+            player_rank: 2
+        }, {
+            player_name: '- Rainbow -',
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 268397,
+            player_mods: [],
+            player_rank: 6,
+        }],
+        blue: [{
+            player_name: '- Rainbow -',
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 403437,
+            player_mods: ['HD'],
+            player_rank: 3,
+        }, {
+            player_name: '- Rainbow -',
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 371937,
+            player_mods: [],
+            player_rank: 4,
+        }, {
+            player_name: '- Rainbow -',
+            player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
+            player_score: 371007,
+            player_mods: [],
+            player_rank: 5,
+        }],
+        none: [{
 
-            },
-        },
-    },
+        }],
+    },{
+        red:[],
+    }],
 
     // 面板文字
     index_powered: 'powered by Yumubot v0.3.0 EA // Match Monitor Now (!ymmn)',
@@ -118,67 +110,6 @@ export async function panel_F (data = {
     index_panel_name: 'MN v3.6',
 
 }, reuse = false) {
-    const scores = [
-        {
-            red: [
-                {
-                    player_name: 'na-gi', //妈的 为什么get match不给用户名啊
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 464277,
-                    player_mods: [],
-                    player_rank: 1, //一局比赛里的分数排名，1v1或者team都一样
-                },
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 412096,
-                    player_mods: [],
-                    player_rank: 2
-                },
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 268397,
-                    player_mods: [],
-                    player_rank: 6,
-                },
-            ],
-            blue: [
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 403437,
-                    player_mods: ['HD'],
-                    player_rank: 3,
-                },
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 371937,
-                    player_mods: [],
-                    player_rank: 4,
-                },
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 371007,
-                    player_mods: [],
-                    player_rank: 5,
-                },
-            ],
-            none: [
-                {
-                    player_name: '- Rainbow -',
-                    player_avatar: 'PanelObject/F_LabelF1_Avatar.png',
-                    player_score: 403437,
-                    player_mods: ['HD'],
-                    player_rank: 3,
-                },
-            ],
-        },
-    ]
-
-
     // 导入模板
     let svg = readTemplate('template/Panel_F.svg');
 
@@ -280,7 +211,7 @@ export async function panel_F (data = {
         }
     }
 
-    await implantRoundLabelF1(data.scores["0"]["red"]["0"]);
+    await implantRoundLabelF1(data.scores["0"]["red"]["2"]);
 
 
     // 导入比赛简介卡（A2卡
