1855c4424f48e07489bee591435f0cecd4000f6e
YuriMumu <1340691940@qq.com>
2023-03-17 01:35:09 +0800

    尝试修正score的bug

--- main.js
+++ main.js
@@ -3,37 +3,12 @@ import formidable from "express-formidable";
 import {CACHE_PATH, getExportFileV3Path, initPath, readImage, readNetImage} from "./src/util.js";
 import {panel_D} from "./src/panel/panel_D.js";
 import {panel_E} from "./src/panel/panel_E.js";
-import fs from "fs";
 
 initPath();
-console.time('E')
-fs.writeFileSync("image/out/panel_E.png", await panel_E());
-console.timeEnd('E')
-
-/*
-console.time()
-console.time('C')
-fs.writeFileSync("image/out/panel_C.png", await panel_C());
-console.timeEnd('C')
-console.time('D')
-fs.writeFileSync("image/out/panel_D.png", await panel_D());
-console.timeEnd('D')
-console.time('E')
-fs.writeFileSync("image/out/panel_E.png", await panel_E());
-console.timeEnd('E')
-console.time('F')
-fs.writeFileSync("image/out/panel_F.png", await panel_F());
-console.timeEnd('F')
-console.time('H')
-fs.writeFileSync("image/out/panel_H.png", await panel_H());
-console.timeEnd('H')
-console.time('I')
-fs.writeFileSync("image/out/panel_I.png", await panel_I());
-console.timeEnd('I')
-console.timeEnd()
- */
+//这里放测试代码
 
 const app = express();
+
 app.use(formidable({
     encoding: 'utf-8', uploadDir: CACHE_PATH, autoClean: true, multiples: true,
 }));
@@ -379,7 +354,6 @@ app.post('/panel_E', async (req, res) => {
             } else judges.push({});
 
             if (n200 && gamemode === 'fruits') { //ctb的n200 （丢小果
-                judges.push({}); //留空格？
                 judges.push({
                     index: 'MD',
                     stat: n200,
