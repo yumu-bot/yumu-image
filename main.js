6eea20dc004dc7d726930169fd9ebaef5150d7a9
YuriMumu <1340691940@qq.com>
2023-04-14 02:23:40 +0800

    mania 分数显示逻辑修正

--- main.js
+++ main.js
@@ -257,7 +257,7 @@ app.post('/panel_E', async (req, res) => {
                 case 'c':
                     return Math.max(n300, n100, n50, n0, n200); //小果miss(katu)也要传过去的
                 case 'm':
-                    return n320 + n300 + n200 + n100 + n50 + n0;
+                    return Math.max(n320 + n300, n200, n100, n50, n0);
                 default:
                     return n320 + n300 + n200 + n100 + n50 + n0;
             }
