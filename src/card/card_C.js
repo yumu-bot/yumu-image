ae468af2345a7c47b5129af684f280211e013302
YuriMumu <1340691940@qq.com>
2023-05-31 03:43:10 +0800

    改变了得点的渲染规则，现在会在得点的正上方渲染，因此不需要改输入的数据了

--- src/card/card_C.js
+++ src/card/card_C.js
@@ -104,13 +104,13 @@ export async function card_C (data = {
             svg = replaceText(svg, red_color_list[0], reg_backcolor);
             red_font = torus;
             blue_font = torusRegular;
-            svg = implantSvgBody(svg, 30 + 16 * data.statistics.wins_team_red_before,176, red_point_plus, reg_redpoint);
+            svg = implantSvgBody(svg, 30 + 16 * Math.max(data.statistics.wins_team_red_before - 1, 0),176, red_point_plus, reg_redpoint);
 
         } else if (data.statistics.is_team_blue_win) {
             svg = replaceText(svg, blue_color_list[0], reg_backcolor);
             red_font = torusRegular;
             blue_font = torus;
-            svg = implantSvgBody(svg, 1338 - 16 * data.statistics.wins_team_blue_before,176, blue_point_plus, reg_bluepoint);
+            svg = implantSvgBody(svg, 1338 - 16 * Math.max(data.statistics.wins_team_blue_before - 1, 0),176, blue_point_plus, reg_bluepoint);
 
         } else {
             red_font = torusRegular;
