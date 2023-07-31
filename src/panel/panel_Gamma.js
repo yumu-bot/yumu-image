c32b2fc6a95e31ba40f751a95ce689239312c91d
YuriMumu <1340691940@qq.com>
2023-08-01 01:53:13 +0800

    panel_Gamma 修改

--- src/panel/panel_Gamma.js
+++ src/panel/panel_Gamma.js
@@ -18,8 +18,8 @@ export async function router(req, res) {
         let routeData = {};
         const classification = req.fields?.panel;
         switch (classification) {
-            case 'info': routeData = await PanelGamma.infoVersion(req.fields); break;
-            case 'score': routeData = await PanelGamma.scoreVersion(req.fields); break;
+            case 'info': routeData = await PanelGamma.infoVersion(req.fields?.user); break;
+            case 'score': routeData = await PanelGamma.scoreVersion(req.fields?.score); break;
         }
 
         const data = await panel_Gamma(routeData);
@@ -91,148 +91,49 @@ export async function panel_Gamma(data = {
 
 const PanelGamma = {
     infoVersion: async (data) => {
-        const background = await readNetImage(data.user?.cover_url || data.user?.cover?.url, getExportFileV3Path('card-default.png'));
-        const avatar = await readNetImage(data.user?.avatar_url || data.user?.avatar?.url, getExportFileV3Path('avatar-guest.png'));
+        const background = await readNetImage(data?.cover_url || data?.cover?.url, getExportFileV3Path('card-default.png'));
+        const avatar = await readNetImage(data?.avatar_url || data?.avatar?.url, getExportFileV3Path('avatar-guest.png'));
 
         return {
             background: background,
             avatar: avatar,
-            mode: data.user.playmode,
-            left1: '#' + data.user.globalRank,
-            left2: data.user.country.countryCode + '#' + data.user.countryRank,
-            left3: data.user.playCount + 'PC',
-            down1: data.user.follower_count + 'Fans',
-            down2: 'u ' + data.user.id,
-            center0b: Math.round(data.user.pp),
+            mode: data.playmode,
+            left1: '#' + data.globalRank,
+            left2: data.country.countryCode + '#' + data.countryRank,
+            left3: data.playCount + 'PC',
+            down1: data.follower_count + 'Fans',
+            down2: 'u ' + data.id,
+            center0b: Math.round(data.pp),
             center0m: 'PP',
-            center1b: data.user.username,
+            center1b: data.username,
             center1m: '',
-            center2: getDecimals(data.user.accuracy, 2) + getDecimals(data.user.accuracy, 3)
-            + '% // Lv.' + data.user.levelCurrent,
+            center2: getDecimals(data.accuracy, 2) + getDecimals(data.accuracy, 3)
+            + '% // Lv.' + data.levelCurrent,
 
             panel: 'info',
         };
     },
 
-    scoreVersion: async (data= {
-        score: {
-            "id": 1343310463,
-            "user_id": 7003013,
-            "accuracy": 0.96875,
-            "mods": [],
-            "score": 851494,
-            "max_combo": 41,
-            "passed": true,
-            "perfect": false,
-            "statistics": {
-                "count_50": 0,
-                "count_100": 0,
-                "count_300": 26,
-                "count_geki": 36,
-                "count_katu": 0,
-                "count_miss": 2
-            },
-            "rank": "S",
-            "created_at": "2021-08-18T12:34:53+00:00",
-            "best_id": 441054683,
-            "pp": null,
-            "mode": "mania",
-            "mode_int": 3,
-            "replay": true,
-            "beatmap": {
-                "beatmapset_id": 1320263,
-                "difficulty_rating": 0.92,
-                "id": 2735119,
-                "mode": "mania",
-                "status": "loved",
-                "total_length": 30,
-                "user_id": 259972,
-                "version": "[10K] [MX00] new beginnings",
-                "accuracy": 0,
-                "ar": 5,
-                "bpm": 130,
-                "convert": false,
-                "count_circles": 64,
-                "count_sliders": 0,
-                "count_spinners": 0,
-                "cs": 10,
-                "deleted_at": null,
-                "drain": 0,
-                "hit_length": 28,
-                "is_scoreable": true,
-                "last_updated": "2021-02-17T09:18:50+00:00",
-                "mode_int": 3,
-                "passcount": 729,
-                "playcount": 1909,
-                "ranked": 4,
-                "url": "https://osu.ppy.sh/beatmaps/2735119",
-                "checksum": "fcd0138d3e6604237298d3f5cdd2f7a7"
-            },
-            "beatmapset": {
-                "artist": "Various Artists",
-                "artist_unicode": "Various Artists",
-                "covers": {
-                    "cover": "https://assets.ppy.sh/beatmaps/1320263/covers/cover.jpg?1622203051",
-                    "cover@2x": "https://assets.ppy.sh/beatmaps/1320263/covers/cover@2x.jpg?1622203051",
-                    "card": "https://assets.ppy.sh/beatmaps/1320263/covers/card.jpg?1622203051",
-                    "card@2x": "https://assets.ppy.sh/beatmaps/1320263/covers/card@2x.jpg?1622203051",
-                    "list": "https://assets.ppy.sh/beatmaps/1320263/covers/list.jpg?1622203051",
-                    "list@2x": "https://assets.ppy.sh/beatmaps/1320263/covers/list@2x.jpg?1622203051",
-                    "slimcover": "https://assets.ppy.sh/beatmaps/1320263/covers/slimcover.jpg?1622203051",
-                    "slimcover@2x": "https://assets.ppy.sh/beatmaps/1320263/covers/slimcover@2x.jpg?1622203051"
-                },
-                "creator": "Jakads",
-                "favourite_count": 268,
-                "hype": null,
-                "id": 1320263,
-                "nsfw": false,
-                "play_count": 73853,
-                "preview_url": "//b.ppy.sh/preview/1320263.mp3",
-                "source": "",
-                "status": "loved",
-                "title": "4+6K Pack 1",
-                "title_unicode": "4+6K Pack 1",
-                "user_id": 259972,
-                "video": false
-            },
-            "user": {
-                "avatar_url": "https://a.ppy.sh/7003013?1627924902.jpeg",
-                "country_code": "CN",
-                "default_group": "default",
-                "id": 7003013,
-                "is_active": true,
-                "is_bot": false,
-                "is_deleted": false,
-                "is_online": true,
-                "is_supporter": true,
-                "last_visit": "2021-08-18T12:33:56+00:00",
-                "pm_friends_only": false,
-                "profile_colour": null,
-                "username": "Muziyami"
-            }
-        },
-
-        panel: "score",
-    }) => {
-        const background = readImage(getExportFileV3Path('object-score-backimage-' + data.score.rank + '.jpg'));
-        const avatar = await readNetImage(data.score.beatmapset.covers["list@2x"], getExportFileV3Path('beatmap-defaultBG.jpg'));
+    scoreVersion: async (data) => {
+        const background = readImage(getExportFileV3Path('object-score-backimage-' + data.rank + '.jpg'));
+        const avatar = await readNetImage(data.beatmapset.covers["list@2x"], getExportFileV3Path('beatmap-defaultBG.jpg'));
 
         return {
             background: background,
             avatar: avatar,
-            mode: data.score.mode,
-            left1: data.score.beatmapset.title,
-            left2: data.score.beatmapset.artist,
-            left3: data.score.beatmap.version,
-            down1: data.score.score,
-            down2: 'b ' + data.score.beatmap.id,
-            center0b: getDecimals(data.score.beatmap.difficulty_rating, 2),
-            center0m: getDecimals(data.score.beatmap.difficulty_rating, 3) + '*',
-            center1b: data.score.pp || '-',
+            mode: data.mode,
+            left1: data.beatmapset.title,
+            left2: data.beatmapset.artist,
+            left3: data.beatmap.version,
+            down1: data.score,
+            down2: 'b ' + data.beatmap.id,
+            center0b: getDecimals(data.beatmap.difficulty_rating, 2),
+            center0m: getDecimals(data.beatmap.difficulty_rating, 3) + '*',
+            center1b: data.pp || '-',
             center1m: 'PP',
-            center2: getDecimals(data.score.accuracy * 100, 2) +
-                getDecimals(data.score.accuracy * 100, 3)
-                + '% // ' + (data.score.max_combo || 0) + 'x',
+            center2: getDecimals(data.accuracy * 100, 2) +
+                getDecimals(data.accuracy * 100, 3)
+                + '% // ' + (data.max_combo || 0) + 'x',
 
             panel: 'score',
         };
