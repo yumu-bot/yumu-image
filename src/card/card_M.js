d6fbec249c846d952770979e65e2397dca810491
YuriMumu <1340691940@qq.com>
2023-08-03 01:19:18 +0800

    compact 渲染方式修改，逻辑写错了

--- src/card/card_M.js
+++ src/card/card_M.js
@@ -388,9 +388,9 @@ export async function card_M(data = {
 
         //插入到svg中
         // 1. compact normal
-        for (let lx = 0; lx < label_compact_x; lx++) {
-            for (let ly = 0; ly < label_compact_y; ly++) {
-                svg = implantSvgBody(svg, 10 + (label_width + 10) * lx, 150 - 60 * ly, labelM1c[ly + lx * 3], reg_label);
+        for (let ly = 0; ly < label_compact_y; ly++) {
+            for (let lx = 0; lx < label_compact_x; lx++) {
+                svg = implantSvgBody(svg, 10 + (label_width + 10) * lx, 150 - 60 * ly, labelM1c[lx + ly * label_compact_x], reg_label);
             }
         }
 
