713cd2c302205ef489f5adfa224ae5085f08a358
YuriMumu <1340691940@qq.com>
2023-08-07 18:18:24 +0800

    panel E0 best显示错误修正

--- src/panel/panel_E0.js
+++ src/panel/panel_E0.js
@@ -370,7 +370,7 @@ async function score2CardE1(score, calcPP) {
 async function score2CardE2(score, calcPP) {
     const isFC = score.perfect || (score.beatmap.max_combo === score.max_combo);
     const isPF = score.rank === 'XH' || score.rank === 'X';
-    const isBest = score.best_id !== null;
+    const isBest = (score.best_id !== null);
 
     return {
         rank: score.rank || 'F',
