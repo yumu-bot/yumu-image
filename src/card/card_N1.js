9441e716f000bb7bf8a196e1c76c7fa3df998b2b
YuriMumu <1340691940@qq.com>
2023-08-23 21:22:04 +0800

    slice

--- src/card/card_N1.js
+++ src/card/card_N1.js
@@ -340,7 +340,7 @@ function getStatColorArr(mode = 'o') {
 }
 
 function getBGOpacity(timeDifference = '-1d') {
-    const unit = timeDifference ? timeDifference.toString().substring(-1, 0) : '-';
+    const unit = timeDifference ? timeDifference.toString().slice(-1) : '-';
     console.log(unit)
     switch (unit) {
         case 'y': return 0.2;
