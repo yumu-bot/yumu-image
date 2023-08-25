59ac3df8ccbf380d6753ab3f5599a1cff55f278f
YuriMumu <1340691940@qq.com>
2023-08-25 21:26:41 +0800

    要用statistics.pp

--- src/panel/panel_A1.js
+++ src/panel/panel_A1.js
@@ -240,10 +240,8 @@ export async function panel_A1(data = {
         const y = Math.floor(i / 4);
         //230825 加上PP附带的背景（maimai限定DX Rating的底色
 
-        const pp = data.friend_card_A1[i].pp;
+        const pp = data.friend_card_A1[i].statistics.pp;
         const glow = getDXRatingBGPath(pp);
-
-        console.log(glow)
 
         svg = implantImage(svg, 510, 290, 470 * x, 290 + 250 * y, 1, glow, reg_friend_card_a1); //放一层试试
         svg = implantSvgBody(svg, 40 + 470 * x, 330 + 250 * y, friend_cardA1s[i], reg_friend_card_a1);
@@ -271,8 +269,7 @@ export async function panel_A1(data = {
 }
 
 function getDXRatingBGPath(pp = 0) {
-    let path;
-
+    let path = '';
     if (typeof pp !== 'number') pp = 0;
 
     if (pp >= 15000) path = 'backlight-rainbow.png';
@@ -286,8 +283,6 @@ function getDXRatingBGPath(pp = 0) {
     else if (pp >= 2000) path = 'backlight-green.png';
     else if (pp >= 1000) path = 'backlight-blue.png';
     else return '';
-
-    console.log(path)
 
     return getExportFileV3Path(path);
 }
