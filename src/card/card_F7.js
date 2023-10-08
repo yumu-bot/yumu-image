365c1377dc5cfd7759cf239939109abec93fec73
YumeMuzi <1340691940@qq.com>
2023-10-09 07:18:16 +0800

    修复 card F7

--- src/card/card_F7.js
+++ src/card/card_F7.js
@@ -2,12 +2,14 @@ import {torus} from "../font.js";
 import {PanelDraw} from "../util.js";
 
 //drawManiaVariantRank, cr country rank, gr global rank
-export function card_F7(mode = 'osu', country = 'CN', pp4k = 0, pp7k = 0, gr4k = 0, gr7k = 0, cr4k = 0, cr7k = 0) {
-    if (mode === 'mania') {
+export function card_F7(data = {
+   mode: 'osu', country: 'CN', pp4k: 0, pp7k: 0, gr4k: 0, gr7k: 0, cr4k: 0, cr7k: 0
+}, reuse = false) {
+    if (data.mode === 'mania') {
 
         //4K: 4396PP // #114514 CN#1919
-        const key4 = '4K: ' + pp4k + 'PP // #' + gr4k + ' ' + country + '#'  + cr4k;
-        const key7 = '7K: ' + pp7k + 'PP // #' + gr7k + ' ' + country + '#'  + cr7k;
+        const key4 = '4K: ' + data.pp4k + 'PP // #' + data.gr4k + ' ' + data.country + '#'  + data.cr4k;
+        const key7 = '7K: ' + data.pp7k + 'PP // #' + data.gr7k + ' ' + data.country + '#'  + data.cr7k;
 
         const maxWidth = Math.max(torus.getTextWidth(key4, 24), torus.getTextWidth(key7, 24));
         const rrect = PanelDraw.Rect(1880 - 40 - maxWidth, 200, maxWidth + 40, 70, 20, '#382E32', 1);
