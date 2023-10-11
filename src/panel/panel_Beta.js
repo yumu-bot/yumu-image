f4f9cd45bf436ad532c14098a48278d2b9b7a699
spring <365246692@qq.com>
2023-10-11 12:13:37 +0800

    update sp.svg

--- src/panel/panel_Beta.js
+++ src/panel/panel_Beta.js
@@ -56,8 +56,6 @@ export async function panel_Beta(score) {
     const url_avatar = await readNetImage(`https://a.ppy.sh/${score.user.id}`);
     const url_bg = await getDiffBG(score.beatmap.id); //`https://assets.ppy.sh/beatmaps/${score.beatmapset.id}/covers/list@2x.jpg`
 
-    console.log(url_avatar);
-    console.log(url_bg);
     const mode_int = score.mode_int;
     const mods = score.mods;
     const rankStr = score.rank;
