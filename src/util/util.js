917139ca6fd1b67db7a0d7f77492145db56c3350
YumeMuzi <1340691940@qq.com>
2025-04-15 03:41:45 +0800

    修改：规范版本

--- src/util/util.js
+++ src/util/util.js
@@ -1835,7 +1835,7 @@ export const getPanelHeight = (cardCount = 0, cardHeight = 110, cardPerRow = 2, 
 }
 
 //公用方法：给面板上名字
-export function getPanelNameSVG(name = '?? (!test)', index = '?', request_time = 'request time: ' + getNowTimeStamp(), version = 'v0.0.0 Dev', color = '#fff', powered = 'Yumubot') {
+export function getPanelNameSVG(name = '?? (!test)', index = '?', request_time = 'request time: ' + getNowTimeStamp(), version = 'v0.5.2 DX', color = '#fff', powered = 'Yumubot') {
 
     // powered by Yumubot v0.5.2 DX // Score (!ymp / !ymr / !yms)
     const powered_text = torus.getTextPath(
