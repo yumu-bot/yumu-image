b346afb564d2c731c7c6dbbc7e8460b85a810f17
YumeMuzi <1340691940@qq.com>
2025-10-13 15:26:34 +0800

    修复：MD Help 可以显示图片了

--- src/component/component_MD.js
+++ src/component/component_MD.js
@@ -1,13 +1,16 @@
 import puppeteer from "puppeteer";
+import path from "path";
+
+const path_util = path;
 
 const browser = await puppeteer.launch({args: ["--disable-web-security", "--disable-features=IsolateOrigins", "--disable-site-isolation-trials"]});
 const page = await browser.newPage();
 
-const path = `${process.cwd()}/template/Card_Alpha/index.html`;
+const template_path = `${process.cwd()}/template/Card_Alpha`;
 
 export async function router(req, res) {
     try {
-        const data = await component_MD(req.fields.md || "# Error: no value", req.fields.width || 600);
+        const data = await component_MD(req.fields.md || "# Error: no value", req.fields.width || 1920);
         res.set('Content-Type', 'image/jpeg');
         res.send(data);
     } catch (e) {
@@ -130,25 +133,58 @@ async function markdownToHTML(markdown) {
 }
 */
 
+function resolveMarkdownImages(markdown = "", markdown_dir = "", template_path = "") {
+
+    const regex = /!\[(.*?)]\((.*?)\)/g;
+
+    return markdown.replace(regex, (match, image_text, image_url) => {
+        // 跳过绝对路径和网络路径
+        if (image_url.startsWith('http://') ||
+            image_url.startsWith('https://') ||
+            image_url.startsWith('/')) {
+            return match;
+        }
+
+        // 解析图片的绝对路径
+        const absolute_image_path = path_util.resolve(markdown_dir, image_url);
+
+        // 计算相对于目标目录的相对路径（使用正斜杠）
+        let relative_path = path_util.relative(template_path, absolute_image_path);
+        relative_path = relative_path.replace(/\\/g, '/'); // 将反斜杠转换为正斜杠
+
+        return `![${image_text}](${relative_path})`;
+    });
 
 
-export async function component_MD(md = "", width = 1080, height = 600) {
+}
+
+
+/**
+ * 获取 md 页面
+ * @param markdown
+ * @param width
+ * @param height
+ * @param markdown_dir 如果这个 markdown 有图片，可以在这里定位这个 markdown 的绝对路径，此时图片才能正常显示
+ * @return {Promise<{image: string, width: number, height: number}>}
+ */
+export async function component_MD(markdown = "", width = 1080, height = 600, markdown_dir = null) {
     await page.setViewport({
         width: width,
         height: height,
         isMobile: false,
     });
-    await page.goto('file://' + path);
+
+    let md
+    if (markdown_dir) {
+        md = resolveMarkdownImages(markdown, markdown_dir, template_path)
+    } else {
+        md = markdown
+    }
+
+    await page.goto('file://' + template_path + "/index.html");
     await page.evaluate(([markdownStr]) => {
         window.setStr(markdownStr);
     }, [md]);
-
-    /*
-    await page.waitForSelector('article', {
-        timeout: 2000
-    });
-
-     */
 
     // 等待网络空闲，确保所有资源加载完成
     await page.waitForNetworkIdle();
