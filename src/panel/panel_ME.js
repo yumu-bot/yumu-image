1c6d4f5f9c15ac00c4525bd9436641e455c1ea41
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-04-04 03:44:47 +0800

    修复：ME 面板没有难度的问题

--- src/panel/panel_ME.js
+++ src/panel/panel_ME.js
@@ -331,7 +331,7 @@ const component_E1 = (
             break;
     }
 
-    const difficulty = (data?.difficulty ?? 0).toString()
+    const difficulty = rounds(data?.difficulty ?? 0).toString()
 
     const text_arr = [
         {
