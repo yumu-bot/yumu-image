fc4270875d56489c404f168a104f8e97019699ee
YumeMuzi <77779996+YumeMuzi@users.noreply.github.com>
2026-06-30 22:50:11 +0800

    修复：null

--- src/panel/panel_E5.js
+++ src/panel/panel_E5.js
@@ -1157,7 +1157,7 @@ const PanelEGenerate = {
 
         if (score?.ruleset_id === 3) {
             max_combo = (score?.maximum_statistics?.perfect ?? 0) + (score?.maximum_statistics?.legacy_combo_increase ?? 0)
-        } else if (score.beatmap.convert === true && score?.ruleset_id === 2) {
+        } else if (score?.beatmap?.convert === true && score?.ruleset_id === 2) {
             // 老 bug，standard 转 catch 的谱面，转盘会被当成 1 个连击，但其实没有连击
             max_combo = (score?.beatmap?.max_combo ?? 0) - (score?.beatmap?.count_spinners ?? 0)
         } else {
